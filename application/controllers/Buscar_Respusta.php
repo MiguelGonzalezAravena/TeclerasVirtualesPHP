6ea64aae169422b4b61ab288ffeb28cf810162e4
Lefua <luis.fernandoo@hotmail.es>
2015-11-26 15:31:01 +0100

    issue#11: prueba

--- application/controllers/Buscar_Respusta.php
+++ application/controllers/Buscar_Respusta.php
@@ -8,9 +8,26 @@ class Controlador1 extends CI_Controller{
 	}
 
 	function index(){
-		$this->load->view('BuscarRespuestas');
-		//$this->load->view('Pagina/footer');
+		$this->load->view('Buscar_Respuesta_vista.php');
 	}
 
+	function validar(){
+		$this->form_validation->set_rules('Seleccione pregunta','required|trim');
+		$this->form_validation->set_rules('date','date','callback_checkDateFormat');
 
+		
+		$this->form_validation->set_message('required','El campo %s Es Obligatorio');
+		
+		if ($this->form_validation->run() == FALSE){
+			//echo "<script de tipo="text/javascript">alerta('Por favor ingrese los valores correctos dd/mm/aaaa Format');</script>" 
+			$this->formulario('Buscar_Respuesta_vista');
+		
+		}
+		else
+		{
+			//echo "Totos los datos estan ok: ".$this->input->post('nombre');
+			
+	}
+
+}
 ?>
---
+++ application/controllers/Prueba.php
@@ -0,0 +1,17 @@
+<?php
+if(!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Prueba extends CI_Controller {
+
+  public function __construct() {
+    parent::__construct();
+    //$this->load->view('prueba_vista');
+    $this->load->database('default');
+  }
+
+ public function index(){
+ 	$this->load->view('prueba_vista');
+ }
+ 
+ }
+?>
