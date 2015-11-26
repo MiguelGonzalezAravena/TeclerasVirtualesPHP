1f987696499ff094de1c403eb93980748a184919
Lefua <luis.fernandoo@hotmail.es>
2015-11-26 19:09:30 +0100

    commit de prueba issue#11

--- application/controllers/Prueba.php
+++ application/controllers/Prueba.php
@@ -5,12 +5,12 @@ class Prueba extends CI_Controller {
 
   public function __construct() {
     parent::__construct();
-    //$this->load->view('prueba_vista');
+    $this->load->view('prueba_vista');
     $this->load->database('default');
   }
 
  public function index(){
- 	$this->load->view('prueba_vista');
+ 	//$this->load->view('prueba_vista');
  }
  
  }
