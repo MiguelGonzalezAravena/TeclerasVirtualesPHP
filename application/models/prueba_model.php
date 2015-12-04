b51a6c5121c62cfaa36e095e6d0d68d497af87ac
Lefua <luis.fernandoo@hotmail.es>
2015-12-04 23:36:40 +0100

    issue#11: mostrar datos

--- application/models/prueba_model.php
+++ application/models/prueba_model.php
@@ -3,32 +3,45 @@ class prueba_model extends CI_Model {
 
 	public function __construct() {
     parent::__construct();
-   //$this->load->view('prueba_vista');
-  // $this->load->model('');
-   $this->load->database('tecleras');
+    $this->load->database('tecleras');
   }
 
   
-  public function verTodo() {
-  	// $consulta = $this->db->query('SELECT PM_NOMBRE FROM tv_pregunta_maestra');
-  	// return $consulta->row_array();
+  public function verTabla_Maestra() {
+    $query = $this->db->get('tv_pregunta_realizada');
+    if ($query->num_rows() > 0)
+      return $query;
+    else
+      return false;
+   
+    return $query->result();
+  }
 
-  //   if ($id === 0) {
-  //     $query = $this->db->get('tv_pregunta_maestra');
-  //     return $query->result_array();
-  //   }
-    
-  //   $query = $this->db->get_where('tv_pregunta_maestra', array('PM_ID' => $id));
-  //   return $query->row_array();
+
+  public function join() {
+        $this->db->select('*');
+        $this->db->from('tv_pregunta_realizada');
+        $this->db->join('tv_pregunta_maestra', 'tv_pregunta_maestra.PM_ID = tv_pregunta_realizada.PM_ID');
+        $this->db->join('tv_respuestas', 'tv_respuestas.PM_ID = tv_pregunta_maestra.PM_ID');
+        $query2 = $this->db->get();
+        if ($query2->num_rows() > 0)
+           return $query2;
+       
+        return false;
+
+       return $query2->result();
+  }
+
+
+  
+
+
+  // public function verTabla_Respuesta() {
+  //      $this->db->select('*');
+  //      $this->db->from('blogs');
+  //      $this->db->join('comments', 'comments.id = blogs.id');
+
+  //   $query = $this->db->get();
   // }
- 
-  $query = $this->db->get('tv_pregunta_maestra');
- 
-  if ($query->num_rows() > 0)
-    return $query;
-  else
-    return false;
- 
-  return $query->result();
-}
+
 }
