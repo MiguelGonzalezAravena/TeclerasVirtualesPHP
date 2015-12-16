02be5713e8a0a115c5f864befae6b3d9e49dbc05
Lefua <luis.fernandoo@hotmail.es>
2015-12-16 07:56:58 +0100

    issue#11= mostrar datos docente

--- application/models/prueba_model.php
+++ application/models/prueba_model.php
@@ -1,47 +1,54 @@
-<?php 
+<?php
 class prueba_model extends CI_Model {
-
-	public function __construct() {
+  public function __construct() {
     parent::__construct();
     $this->load->database('tecleras');
   }
 
-  
-  public function verTabla_Maestra() {
-    $query = $this->db->get('tv_pregunta_realizada');
-    if ($query->num_rows() > 0)
-      return $query;
-    else
-      return false;
-   
+   function get_pregunta($datos){ //obtiene los datos de la tabla marter, solo del ID seleccionado
+    $sql= "SELECT PM_ID,PM_TEXTO,PM_FECHA_CREACION,PM_EXPLICACION FROM tv_pregunta_maestra where PM_ID = ?";
+    $query = $this->db->query($sql,array($datos['PM_ID']));
     return $query->result();
-  }
+}
+   function get_datos_pregunta($datos){ //obtiene los datos de la tabla respuestas, solo del ID seleccionado
+    $sql2= "SELECT RES_TEXTO,PM_CORRECTA FROM tv_respuestas where PM_ID = ?";
+    $query2 = $this->db->query($sql2,array($datos['PM_ID']));
+    return $query2->result();
+}
+  // public function respuesta() {
+  //   $this->db->like('PM_ID', $this->input->get('pm_nombre'));
+  //   $query=$this->db->get('tv_respuestas');
+  //   return $query;
+  //   //$query = $this->db->get('tv_respuestas');
+  //   // $this->db->select('*');
+  //   // $this->db->from('tv_respuestas');
+  //   // $query = $this->db->get();
+  //   // if ($query->num_rows() > 0)
+  //   //   return $query;
+  //   // else
+  //   //   return false;
+   
+  //   //return $query->result();
+  // }
 
-
-  public function join() {
-        $this->db->select('*');
-        $this->db->from('tv_pregunta_realizada');
-        $this->db->join('tv_pregunta_maestra', 'tv_pregunta_maestra.PM_ID = tv_pregunta_realizada.PM_ID');
-        $this->db->join('tv_respuestas', 'tv_respuestas.PM_ID = tv_pregunta_maestra.PM_ID');
-        $query2 = $this->db->get();
-        if ($query2->num_rows() > 0)
-           return $query2;
+  // public function join() {
+  //       $this->db->select('*');
        
-        return false;
-
-       return $query2->result();
-  }
-
-
+  //       $this->db->from('tv_pregunta_realizada');
+  //       $this->db->join('tv_pregunta_maestra', 'tv_pregunta_maestra.PM_ID = tv_pregunta_realizada.PM_ID');
+  //       $this->db->join('tv_respuestas', 'tv_respuestas.PM_ID = tv_pregunta_maestra.PM_ID');
+  //       $query2 = $this->db->get();
+  //       if ($query2->num_rows() > 0)
+  //          return $query2;
+  //      else
+  //       return false;
+  //      return $query2->result();
+  // }
   
-
-
   // public function verTabla_Respuesta() {
   //      $this->db->select('*');
   //      $this->db->from('blogs');
   //      $this->db->join('comments', 'comments.id = blogs.id');
-
   //   $query = $this->db->get();
   // }
-
 }
--- application/views/prueba_vista.php
+++ application/views/prueba_vista.php
@@ -1,38 +1,75 @@
-<div style="text-align:center;"> 
-  <table style="border:1px solid black;">
-  	<tr>
-  		<?php
-  		  $count=0;
-         
-           foreach ($join->result() as $row2){
-             $arreglo=explode(' ',$row2->PR_HORA_INICIO);
-             $alternativas=explode(',',$row2->PM_TEXTO);
-               if($count<1){
-                   echo '<h2> Pregunta ' . $row2->PM_ID. ', Clase '.$arreglo[0] .'<br><h5>Realizada a las '.$arreglo[1].'';
-                   echo '<br></br>';
-                   echo '<p></p>'.$row2->PM_NOMBRE .'<br>';
-                   echo '<br> ►'.$alternativas[0].'<br>►'.$alternativas[1]. '<br>►'.$alternativas[2]. '</br>';
-                   echo '<br> La respuesta correcta es: <font color="red"> '.$row2->PM_CORRECTA.' </font><br>';
-                   echo '<p></p>'.$row2->PM_EXPLICACION;
-                     }
-                    $count++;
-         } 
+<h1><font color="Navyblue"> Docente </font></h1>
+ <div class="panel panel-default" >
+
+  <div class="panel-body">
+    <table style="border:1px solid black;">
+      <tr>
+
+
+<?php
+ $correcta=1; 
+ foreach($arrPregunta as $row){ 
+      $arreglo=explode(' ',$row->PM_FECHA_CREACION);//tabla_maestra
+       echo '<h2> Pregunta ' .$row->PM_ID. ', Clase '.$arreglo[0] .'<br><h5>Realizada a las '.$arreglo[1].'';
+       echo '<br></br>';
+       echo '<p></p>'.$row->PM_TEXTO.'<br>';//pregunta
+       foreach ($arrDatos as $row2){  //este for, muestra todas las alternativas del ID correspondiente
+                             echo '<br> ►'.$row2->RES_TEXTO;
+                             if($correcta == $row2->PM_CORRECTA)//revisa cual es la alternativa correspondiente...
+                               $CORRECTA=$row2->RES_TEXTO;//sabiendo que PM_CORRECTA 0 si es falta y 1 si es correcta
+                  } 
+        echo '<br></br>';
+        echo '<p> La respuesta correcta es: <font color="red"> '.$CORRECTA.' </font></p>';//respondida
+       echo '<p>'.$row->PM_EXPLICACION.'</p>';
+  
+      }
+
+      
+        ?> 
+   
+   </tr>
+  </table>
+</div>
+</div>
+<!-- <div class="panel panel-default" >
+  <div class="panel-body">
+    <table style="border:1px solid black;">
+      <tr> -->
+         <?php
+        //   $count=0;
+        //   $correcta=1; 
+        //   $num=0;
+        //     foreach ($join->result() as $row2){
+        //        $arreglo=explode(' ',$row2->PR_HORA_INICIO);//realizada
+        //         //$alternativas=explode(',',$row2->RES_ID);//maestra
+        //        $talter=count($row2->RES_ID)-1;
+        //        if($count<1){
+        //            echo '<h2> Pregunta ' . $row2->PM_ID. ', Clase '.$arreglo[0] .'<br><h5>Realizada a las '.$arreglo[1].'';
+        //            echo '<br></br>';
+        //            echo '<p></p>'.$row2->PM_TEXTO.'<br>';//pregunta
+        //            foreach ($respuesta->result() as $row){  
+        //                     echo '<br> ►'.$row->RES_TEXTO;
+        //                     if($correcta == $row->PM_CORRECTA)
+        //                       $CORRECTA=$row->RES_TEXTO;
+        //            } 
+        //            echo '<p> La respuesta correcta es: <font color="red"> '.$CORRECTA.' </font></p>';//respondida
+        //            echo '<p></p>'.$row2->PM_EXPLICACION.'<br>';//maestra
+        //         }
+        //             $count++;
+                    
+        //  } 
         // foreach ($enlace->result() as $row) {
         //   $arreglo=explode(' ',$row->PR_HORA_INICIO);
         //   if($count<1){
         //       echo '<h2> Pregunta ' . $row->PM_ID. ', Clase '.$arreglo[0] .'<br><h5>Realizada a las '.$arreglo[1].'';
         //       echo '<br></br>';
-
         //   }
         //   $count++;
         // }
 
-
-          ?>
-          
-  	</tr>
+        ?>
+         
+<!--     </tr>
   </table>
 </div>
-
-
-
+</div> -->
